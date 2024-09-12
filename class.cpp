ee570aaf2d10bcdf4f742da516b9404e7eb280b1
knrbokhari <kazinaeemrayhan@gmail.com>
2024-09-12 22:25:57 +0600

    first commit

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int id;
+    string name;
+    int age;
+    string className;
+};
+
+int main()
+{
+    cout << "hello world!" << endl;
+
+    return 0;
+}
